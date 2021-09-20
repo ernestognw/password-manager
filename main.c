eb9e4824756b233594a5fe154665f040a3a18dc3
EmilianoAncer <a00823377@itesm.mx>
2021-09-20 16:50:07 -0500

    Added genPassword and fixed array sizes

--- main.c
+++ main.c
@@ -4,33 +4,43 @@
 #include <time.h>
 #include <unistd.h>
 
-//const char* ENTRY_FORMAT_OUT = "(%s,%s,%s,%s)\n";
 FILE *f;
 
-
-
+//Entry object
 typedef struct Entry
 {
-  char name[20];
-  char email[15];
-  char password[20];
-  char description[50];
-  char url[20];
+  char name[100];
+  char email[100];
+  char password[51];
+  char description[150];
+  char url[150];
 } Entry;
 
+//random password generator
+char *genPassword(int passSize)
+{
+  char *password = malloc(passSize * sizeof(char));
+  for (int i = 0; i < passSize; i++)
+  {
+    password[i] = 33 + (random() % 90);
+  }
+  return password;
+}
+
+//Create master password
 int crearPasswowrd()
 {
-    char password[15];
-    printf("Crear password:");
-    scanf("%s", password);
-   
-    f = fopen("password.bin", "wb");
-    //fprintf(f, "%s", password);
-    fwrite(password, 15, 1, f);
-    fclose(f);
-    system("clear");
-    return 0;
+  char password[51];
+  printf("Crear password:");
+  scanf("%s", password);
+
+  f = fopen("password.bin", "wb");
+  fwrite(password, 51, 1, f);
+  fclose(f);
+  system("clear");
+  return 0;
 }
+
 void listEntries()
 {
   Entry *e;
@@ -39,12 +49,10 @@ void listEntries()
   FILE *fp;
   fp = fopen("myentries.bin", "rb");
 
+  while (fread(e, sizeof(Entry), 1, fp))
+  {
 
-  while(fread(e,sizeof(Entry),1,fp)){
-    
-       printf("Entry Name: %s\n\n", e->name);
-
-    
+    printf("Entry Name: %s\n\n", e->name);
   }
   fclose(fp);
 }
@@ -53,7 +61,8 @@ void createEntry()
 {
   Entry *e;
   FILE *fp;
-  char *p = malloc(1024);
+  char *p = malloc(1024), *r;
+  int passSize;
 
   e = (Entry *)malloc(sizeof(Entry));
   fp = fopen("myentries.bin", "ab");
@@ -62,7 +71,6 @@ void createEntry()
 
   printf("Enter Entry name\n");
   scanf("%s", p);
-  //e->name = (char *)malloc(sizeof(p));
   strcpy(e->name, p);
   fflush(stdin);
 
@@ -70,7 +78,6 @@ void createEntry()
 
   printf("Enter you username/email\n");
   scanf("%s", p);
-  //e->email = (char *)malloc(sizeof(p));
   strcpy(e->email, p);
   fflush(stdin);
 
@@ -78,88 +85,99 @@ void createEntry()
 
   printf("Enter your password or press 'R' to create random password\n");
   scanf("%s", p);
-  //e->password = (char *)malloc(sizeof(p));
-  strcpy(e->password, p);
-  fflush(stdin);
-
+  if (*p == 'R' || *p == 'r')
+  {
+    printf("Enter the password size (min 6, max 30)\n");
+    scanf("%d", &passSize);
+    r = genPassword(passSize);
+    strcpy(e->password, r);
+    free(r); //free password variable from genPassword
+    fflush(stdin);
+  }
+  else
+  {
+    strcpy(e->password, p);
+    fflush(stdin);
+  }
 
   printf("Enter a description *Optional Press '0' to skip\n");
   scanf("%s", p);
-  if(*p == '0'){
+  if (*p == '0')
+  {
     printf("No description added\n");
-  } else{
-    //e->description = (char *)malloc(sizeof(p));
+  }
+  else
+  {
     strcpy(e->description, p);
     fflush(stdin);
-
   }
-
-  //printf("Description: %s\n", p);
-  
 
   printf("Enter a url *Optional Press '0' to skip\n");
-  scanf("%s",p);
-  if (*p == '0'){
+  scanf("%s", p);
+  if (*p == '0')
+  {
     printf("No Url added\n");
-
-  } else{
-    //e->url = (char *)malloc(sizeof(p));
+  }
+  else
+  {
     strcpy(e->url, p);
   }
-  
 
   fwrite(e, sizeof(Entry), 1, fp);
 
   fclose(fp);
-
-    
 }
 
+void selectedEntry(char *name)
+{
+  Entry *e;
+  e = (Entry *)malloc(sizeof(Entry));
+  int value;
 
-
-
-void selectedEntry(char *name){
-    Entry *e;
-    e = (Entry *)malloc(sizeof(Entry));
-    int value;
-
-    FILE *fp;
-    fp = fopen("myentries.bin", "rb");
-    while(fread(e,sizeof(Entry),1,fp)){
-         value=strcmp(e->name,name);  
-      if(value == 0){
-       printf("Username: %s Username/email: %s Password: %s Description: %s Url: %s \n", e->name, e->email, e->password, e->description, e->url);
-
-      }
-    
+  FILE *fp;
+  fp = fopen("myentries.bin", "rb");
+  while (fread(e, sizeof(Entry), 1, fp))
+  {
+    value = strcmp(e->name, name);
+    if (value == 0)
+    {
+      printf("Username: %s Username/email: %s Password: %s Description: %s Url: %s \n", e->name, e->email, e->password, e->description, e->url);
+    }
   }
   fclose(fp);
+}
 
+void deleteEntry(char *name)
+{
+  Entry *e;
+  e = (Entry *)malloc(sizeof(Entry));
+  FILE *fp;
+  FILE *temp;
+  fp = fopen("myentries.bin", "rb");
+  temp = fopen("tmp.bin", "wb");
+  while (fread(e, sizeof(Entry), 1, fp))
+  {
+    if (strcmp(e->name, name) == 0)
+    {
+      printf("A record with requested name found and deleted.\n\n");
+    }
+    else
+    {
+      fwrite(e, sizeof(Entry), 1, temp);
+    }
   }
-void deleteEntry(char *name){
-   Entry *e;
-   e = (Entry *)malloc(sizeof(Entry));
-   FILE *fp;
-   FILE *temp;
-   fp=fopen("myentries.bin", "rb");
-   temp=fopen("tmp.bin", "wb");
-   while (fread(e,sizeof(Entry),1,fp)) {
-		if (strcmp (e->name, name) == 0) {
-			printf("A record with requested name found and deleted.\n\n");
-			
-		} else {
-			fwrite(e, sizeof(Entry), 1, temp);
-		}
-	}
   fclose(fp);
-	fclose(temp);
+  fclose(temp);
 
-	remove("myentries.bin");
-	rename("tmp.bin", "myentries.bin");
- }
-void menuEntries(){
+  remove("myentries.bin");
+  rename("tmp.bin", "myentries.bin");
+}
+
+void menuEntries()
+{
   int option;
   char *nameEntry = malloc(1024);
+  srand(time(NULL));
 
   do
   {
@@ -190,60 +208,54 @@ void menuEntries(){
       scanf("%s", nameEntry);
       selectedEntry(nameEntry);
     }
-    
-
 
   } while (option != 0);
-
-
 }
-void confirmarContrasena(){
 
-    char password[15];
-    char master_password[15];
-    char entradas[1000];
+//Check master password inputed to stored one
+void confirmarContrasena()
+{
 
-    //Si esto existe, quiere decir que si hay una
-    //master password
-    if (access("password.bin", F_OK) == 0)
+  char password[15];
+  char master_password[15];
+  char entradas[1000];
+
+  //Si esto existe, quiere decir que si hay una
+  //master password
+  if (access("password.bin", F_OK) == 0)
+  {
+    //pido por esa master password
+    printf("Introducir password: ");
+    scanf("%s", password);
+    //Abro el archivo para ver ver si lo que hay dentro  == password introducida
+    f = fopen("password.bin", "rb");
+    // Lo que hay (master password FILE .bin) lo voy a  en master_password
+    fscanf(f, "%s", master_password);
+    //Tengo que cerrar el archivo
+    fclose(f);
+    system("clear");
+
+    if (strcmp(password, master_password) == 0)
     {
-        //pido por esa master password
-        printf("Introducir password: ");
-        scanf("%s", password);
-        //Abro el archivo para ver ver si lo que hay dentro  == password introducida
-        f = fopen("password.bin", "rb");
-        // Lo que hay (master password FILE .bin) lo voy a  en master_password
-        //fscanf(f, "%s", master_password);
-        fread(master_password,15,1,f);
-        //Tengo que cerrar el archivo
-        fclose(f);
-        system("clear");
 
-        if (strcmp(password, master_password) == 0)
-        {
-
-          menuEntries();
-
-        }
-        else
-        {
-            printf("Password no coincide...");
-            //esto es lo mismo que un return -1;
-            exit(EXIT_FAILURE);
-        }
+      menuEntries();
     }
     else
     {
-        crearPasswowrd();
+      printf("Password no coincide...");
+      //esto es lo mismo que un return -1;
+      exit(EXIT_FAILURE);
     }
-
+  }
+  else
+  {
+    crearPasswowrd();
+  }
 }
-
 
 int main()
 {
+  confirmarContrasena();
 
-confirmarContrasena();
-
-return 0;
+  return 0;
 };
